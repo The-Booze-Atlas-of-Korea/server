3b72497bd924c95c0aa6aa0793a6a210ecfee597
son <son2044@gmail.com>
2025-12-01 09:36:20 +0900

    first

---
+++ src/main/java/com/ssafy/sulmap/SulmapApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.sulmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SulmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/sulmap/SulmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.sulmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
