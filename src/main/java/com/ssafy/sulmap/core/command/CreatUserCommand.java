2f51141e92860867f14ff5d542578aff3f406b0a
son <son2044@gmail.com>
2025-12-02 16:06:19 +0900

    add lombok

--- src/main/java/com/ssafy/sulmap/core/command/CreatUserCommand.java
+++ src/main/java/com/ssafy/sulmap/core/command/CreatUserCommand.java
@@ -1,5 +1,10 @@
 package com.ssafy.sulmap.core.command;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class CreatUserCommand {
     String loginId;
     String passwordHash;
