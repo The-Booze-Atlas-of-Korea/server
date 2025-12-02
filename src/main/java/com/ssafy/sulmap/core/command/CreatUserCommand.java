991ca520e7f098a0ddb9408fa2447589e3ed9b4f
son <son2044@gmail.com>
2025-12-02 16:02:39 +0900

    add CreateUserCommand

---
+++ src/main/java/com/ssafy/sulmap/core/command/CreatUserCommand.java
@@ -0,0 +1,12 @@
+package com.ssafy.sulmap.core.command;
+
+public class CreatUserCommand {
+    String loginId;
+    String passwordHash;
+    String name;
+    String email;
+    String phone;
+    String address;
+    String birthday;
+    String gender;
+}
