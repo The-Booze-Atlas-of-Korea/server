b6b4028aa8adcfb36ef819511f3f594a6b61398c
son <son2044@gmail.com>
2025-12-04 19:09:21 +0900

    add userModel

---
+++ src/main/java/com/ssafy/sulmap/core/model/enums/UserAuthProvider.java
@@ -0,0 +1,20 @@
+package com.ssafy.sulmap.core.model.enums;
+
+public enum UserAuthProvider {
+    LOCAL,
+    GOOGLE;
+
+    public static UserAuthProvider fromValue(String value)
+    {
+        return switch (value) {
+            case "LOCAL" -> LOCAL;
+            case "GOOGLE" -> GOOGLE;
+            default -> throw new IllegalStateException("Unexpected value: " + value);
+        };
+    }
+
+    public String getValue()
+    {
+        return this.name();
+    }
+}
