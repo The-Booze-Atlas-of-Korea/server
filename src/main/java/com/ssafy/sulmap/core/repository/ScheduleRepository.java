acf133f0fe15119fbd4903e0d4133a19d043597a
sjkim1070 <sjkim1070@gmail.com>
2025-12-12 17:45:35 +0900

    add ScheduleRepository

---
+++ src/main/java/com/ssafy/sulmap/core/repository/ScheduleRepository.java
@@ -0,0 +1,15 @@
+package com.ssafy.sulmap.core.repository;
+
+import com.ssafy.sulmap.core.model.DrinkingScheduleModel;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface ScheduleRepository {
+    DrinkingScheduleModel save(DrinkingScheduleModel schedule);
+
+    Optional<DrinkingScheduleModel> findById(Long id);
+
+    List<DrinkingScheduleModel> findByPeriod(Long userId, LocalDateTime start, LocalDateTime end);
+}
