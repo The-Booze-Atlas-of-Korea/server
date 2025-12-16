66047c5ad8187367207ba379a41a8a14132feb5f
sjkim1070 <sjkim1070@gmail.com>
2025-12-16 21:21:05 +0900

    fix PlanService return Result<>

--- src/main/java/com/ssafy/sulmap/core/service/PlanService.java
+++ src/main/java/com/ssafy/sulmap/core/service/PlanService.java
@@ -3,11 +3,12 @@ package com.ssafy.sulmap.core.service;
 import com.ssafy.sulmap.core.model.DrinkingPlanModel;
 import com.ssafy.sulmap.core.model.command.CreatePlanCommand;
 import com.ssafy.sulmap.core.model.command.UpdatePlanCommand;
+import com.ssafy.sulmap.share.result.Result;
 
 public interface PlanService {
-    DrinkingPlanModel createPlan(CreatePlanCommand command);
+    Result<DrinkingPlanModel> createPlan(CreatePlanCommand command);
 
-    DrinkingPlanModel updatePlan(UpdatePlanCommand command);
+    Result<DrinkingPlanModel> updatePlan(UpdatePlanCommand command);
 
-    DrinkingPlanModel getPlan(Long planId);
+    Result<DrinkingPlanModel> getPlan(Long planId);
 }
