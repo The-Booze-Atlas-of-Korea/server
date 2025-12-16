039032fc15970c81ca5768c6b521dacd1793f34f
sjkim1070 <sjkim1070@gmail.com>
2025-12-16 21:48:50 +0900

    fix ScheduleService

--- src/main/java/com/ssafy/sulmap/core/service/ScheduleService.java
+++ src/main/java/com/ssafy/sulmap/core/service/ScheduleService.java
@@ -4,11 +4,16 @@ import com.ssafy.sulmap.core.model.DrinkingScheduleModel;
 import com.ssafy.sulmap.core.model.command.CreateScheduleCommand;
 import com.ssafy.sulmap.core.model.command.UpdateScheduleCommand;
 import com.ssafy.sulmap.core.model.query.GetSchedulesInPeriodQuery;
+import com.ssafy.sulmap.share.result.Result;
+
 import java.util.List;
 
 public interface ScheduleService {
-    DrinkingScheduleModel createSchedule(CreateScheduleCommand command);
-    DrinkingScheduleModel updateSchedule(UpdateScheduleCommand command);
-    DrinkingScheduleModel getSchedule(Long scheduleId);
-    List<DrinkingScheduleModel> getSchedulesInPeriod(GetSchedulesInPeriodQuery query);
+    Result<DrinkingScheduleModel> createSchedule(CreateScheduleCommand command);
+
+    Result<DrinkingScheduleModel> updateSchedule(UpdateScheduleCommand command);
+
+    Result<DrinkingScheduleModel> getSchedule(Long scheduleId);
+
+    Result<List<DrinkingScheduleModel>> getSchedulesInPeriod(GetSchedulesInPeriodQuery query);
 }
