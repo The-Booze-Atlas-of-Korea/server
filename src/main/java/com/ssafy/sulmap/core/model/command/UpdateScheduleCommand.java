9ba2aae06cfee79944a97f0861287270283a8caa
gpsrlin115 <lefd123@google.com>
2025-12-18 21:26:50 +0900

    fix UpdateScheduleCommand

--- src/main/java/com/ssafy/sulmap/core/model/command/UpdateScheduleCommand.java
+++ src/main/java/com/ssafy/sulmap/core/model/command/UpdateScheduleCommand.java
@@ -1,13 +1,16 @@
 package com.ssafy.sulmap.core.model.command;
 
 import com.ssafy.sulmap.core.model.enums.ScheduleStatus;
+
+import lombok.Builder;
+
 import java.time.LocalDateTime;
 
+@Builder
 public record UpdateScheduleCommand(
-        Long scheduleId,
-        Long userId, // For ownership check
-        String scheduleTitle,
-        LocalDateTime meetAt,
-        ScheduleStatus status
-) {
+                Long scheduleId,
+                Long userId, // For ownership check
+                String scheduleTitle,
+                LocalDateTime meetAt,
+                ScheduleStatus status) {
 }
