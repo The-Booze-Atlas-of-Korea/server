069418349546d457816f25806ff7d3cf83fa7a2e
son <son2044@gmail.com>
2025-12-18 12:07:32 +0900

    add sql

---
+++ db/init/03_bar_categories.sql
@@ -0,0 +1,13 @@
+CREATE TABLE bar_categories
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50) NOT NULL,
+    group_name VARCHAR(50) NULL,
+
+    created_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at DATETIME NULL,
+
+    UNIQUE KEY uk_bar_categories_name (name),
+    INDEX      idx_bar_categories_deleted_at (deleted_at)
+);
---
+++ db/init/04_bar_category_mapping.sql
@@ -0,0 +1,19 @@
+CREATE TABLE bar_category_mapping
+(
+    id          BIGINT PRIMARY KEY AUTO_INCREMENT,
+    bar_id      BIGINT   NOT NULL,
+    category_id BIGINT   NOT NULL,
+    priority    INT NULL,
+
+    created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at  DATETIME NULL,
+
+    CONSTRAINT fk_bcm_bar FOREIGN KEY (bar_id) REFERENCES bars (id),
+    CONSTRAINT fk_bcm_category FOREIGN KEY (category_id) REFERENCES bar_categories (id),
+
+    UNIQUE KEY uk_bcm_bar_category (bar_id, category_id),
+    INDEX       idx_bcm_bar_id (bar_id),
+    INDEX       idx_bcm_category_id (category_id),
+    INDEX       idx_bcm_deleted_at (deleted_at)
+);
