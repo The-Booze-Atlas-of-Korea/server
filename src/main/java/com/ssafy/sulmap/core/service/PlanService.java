68de96548deb6621d3c917f8d812a6fc1e075eb9
gpsrlin115 <lefd123@gmail.com>
2025-12-21 20:31:36 +0900

    fix PlanService

--- src/main/java/com/ssafy/sulmap/core/service/PlanService.java
+++ src/main/java/com/ssafy/sulmap/core/service/PlanService.java
@@ -4,6 +4,7 @@ import com.ssafy.sulmap.core.model.DrinkingPlanModel;
 import com.ssafy.sulmap.core.model.command.CreatePlanCommand;
 import com.ssafy.sulmap.core.model.command.UpdatePlanCommand;
 import com.ssafy.sulmap.share.result.Result;
+import java.util.List;
 
 public interface PlanService {
     Result<DrinkingPlanModel> createPlan(CreatePlanCommand command);
@@ -11,4 +12,6 @@ public interface PlanService {
     Result<DrinkingPlanModel> updatePlan(UpdatePlanCommand command);
 
     Result<DrinkingPlanModel> getPlan(Long planId);
+
+    Result<List<DrinkingPlanModel>> listPlans(Long ownerUserId, int page, int size, String sort);
 }
