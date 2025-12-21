8c13cfd0224cc7cb60e3e1612720176654c2251f
gpsrlin115 <lefd123@gmail.com>
2025-12-21 20:49:39 +0900

    fix PlanRepository

--- src/main/java/com/ssafy/sulmap/core/repository/PlanRepository.java
+++ src/main/java/com/ssafy/sulmap/core/repository/PlanRepository.java
@@ -1,10 +1,12 @@
 package com.ssafy.sulmap.core.repository;
 
 import com.ssafy.sulmap.core.model.DrinkingPlanModel;
+import java.util.List;
 import java.util.Optional;
 
 public interface PlanRepository {
     DrinkingPlanModel save(DrinkingPlanModel plan);
     Optional<DrinkingPlanModel> findById(Long id);
     void delete(Long id);
+    List<DrinkingPlanModel> findByOwnerUserId(Long ownerUserId, int offset, int limit, String sort);
 }
