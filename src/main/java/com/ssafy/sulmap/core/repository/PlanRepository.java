25ac2124005327ede4a1fe102fe4fafa5b98bc5d
gpsrlin115 <114369169+gpsrlin115@users.noreply.github.com>
2025-12-23 11:19:06 +0900

    add PlanRepository

--- src/main/java/com/ssafy/sulmap/core/repository/PlanRepository.java
+++ src/main/java/com/ssafy/sulmap/core/repository/PlanRepository.java
@@ -1,10 +1,15 @@
 package com.ssafy.sulmap.core.repository;
 
 import com.ssafy.sulmap.core.model.DrinkingPlanModel;
+import java.util.List;
 import java.util.Optional;
 
 public interface PlanRepository {
     DrinkingPlanModel save(DrinkingPlanModel plan);
+
     Optional<DrinkingPlanModel> findById(Long id);
+
+    List<DrinkingPlanModel> findByOwnerUserId(Long ownerUserId);
+
     void delete(Long id);
 }
