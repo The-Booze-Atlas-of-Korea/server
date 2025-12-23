27351668fe572143a1c366cb5cb9360f319b5bdb
gpsrlin115 <114369169+gpsrlin115@users.noreply.github.com>
2025-12-23 16:45:51 +0900

    fix ScheduleRepository for history

--- src/main/java/com/ssafy/sulmap/core/repository/ScheduleRepository.java
+++ src/main/java/com/ssafy/sulmap/core/repository/ScheduleRepository.java
@@ -12,4 +12,6 @@ public interface ScheduleRepository {
     Optional<DrinkingScheduleModel> findById(Long id);
 
     List<DrinkingScheduleModel> findByPeriod(Long userId, LocalDateTime start, LocalDateTime end);
+
+    List<DrinkingScheduleModel> findByOwnerUserIdPaged(Long userId, int offset, int limit);
 }
