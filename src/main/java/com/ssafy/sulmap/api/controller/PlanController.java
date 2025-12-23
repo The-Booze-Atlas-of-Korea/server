3cafb279a5867f362f907664ea5d0a29df24e5cd
gpsrlin115 <114369169+gpsrlin115@users.noreply.github.com>
2025-12-23 11:18:08 +0900

    fix PlanController listPlan endPoints

--- src/main/java/com/ssafy/sulmap/api/controller/PlanController.java
+++ src/main/java/com/ssafy/sulmap/api/controller/PlanController.java
@@ -3,6 +3,7 @@ package com.ssafy.sulmap.api.controller;
 import com.ssafy.sulmap.api.dto.request.CreatePlanRequest;
 import com.ssafy.sulmap.api.dto.request.PlanSpotRequest;
 import com.ssafy.sulmap.api.dto.request.UpdatePlanRequest;
+import com.ssafy.sulmap.api.dto.response.PlanListResponse;
 import com.ssafy.sulmap.api.dto.response.PlanResponse;
 import com.ssafy.sulmap.api.security.model.UserDetail;
 import com.ssafy.sulmap.core.model.DrinkingPlanSpotModel;
@@ -50,6 +51,25 @@ public class PlanController {
         }
 
         return ResponseEntity.ok(PlanResponse.fromModel(result.getOrThrow()));
+    }
+
+    /**
+     * 플랜 목록 조회 (사용자별)
+     */
+    @GetMapping
+    public ResponseEntity<?> listPlans(@AuthenticationPrincipal UserDetail userDetail) {
+        var userId = userDetail.userModel().getId();
+
+        var result = _planService.listPlans(userId);
+        if (result.isFailure()) {
+            return new ResponseEntity<>(result.getSingleErrorOrThrow().getStatus());
+        }
+
+        var plans = result.getOrThrow().stream()
+                .map(PlanListResponse::fromModel)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(plans);
     }
 
     /**
