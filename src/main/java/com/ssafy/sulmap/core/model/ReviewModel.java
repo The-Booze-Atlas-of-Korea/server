d103544c6cdbcafc29c90e2ba084b27b99844b96
gpsrlin115 <114369169+gpsrlin115@users.noreply.github.com>
2025-12-24 14:33:54 +0900

    add ReviewModel

--- src/main/java/com/ssafy/sulmap/core/model/ReviewModel.java
+++ src/main/java/com/ssafy/sulmap/core/model/ReviewModel.java
@@ -1,14 +1,27 @@
 package com.ssafy.sulmap.core.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 리뷰 도메인 모델
+ */
+@Getter
+@Setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class ReviewModel {
     private Long id;
+    private Long userId;
+    private Long barId;
+    private Long visitId;
+    private Integer rating;
+    private String content;
+    private List<ReviewMediaModel> media;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 }
