446de04c7e211182c285b46106f69fd224d6080a
gpsrlin115 <114369169+gpsrlin115@users.noreply.github.com>
2025-12-24 14:36:23 +0900

    add MemoModel

--- src/main/java/com/ssafy/sulmap/core/model/MemoModel.java
+++ src/main/java/com/ssafy/sulmap/core/model/MemoModel.java
@@ -1,5 +1,23 @@
 package com.ssafy.sulmap.core.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * 개인 메모 도메인 모델
+ */
+@Getter
+@Setter
+@Builder
 public class MemoModel {
     private Long id;
+    private Long userId;
+    private Long barId;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 }
